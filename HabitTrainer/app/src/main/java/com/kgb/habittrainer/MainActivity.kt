897ef26ee6e87b7d99887ba6536f7d76cd550b65
Krzysztof Betlej <k.betlej@samsung.local>
2018-05-15 12:05:34 +0200

    Section 6 lesson 71 : Configure application; Create first Activity using kotlin; use kotlin android extension to better access to xml fields

---
+++ HabitTrainer/app/src/main/java/com/kgb/habittrainer/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kgb.habittrainer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv_description.text = "A refreshing glass of watter gets you hydrated"
+    }
+}
